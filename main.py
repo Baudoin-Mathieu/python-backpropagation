d2083e7249af89a316e14269c450398a84c28c04
baudoin mathieu <baudoin.mathieu6@gmail.com>
2024-12-02 15:42:50 +0100

    Added initial files

---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+    
+# input dataset
+X = np.array([  [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1] ])
+
+print("Input Dataset:")
+print(X)
+    
+# output dataset            
+y = np.array([[0,0,1,1]]).T # Transpose to turn the matrix to [[0],[0],[1],[1]]
+
+print("Expected Output Dataset:")
+print(y)
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+
+
+iterNbr = (int)(input("Number of iterations : "))
+
+
+for iter in range(iterNbr):
+
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print("Weights of synapse 0 After Training :")
+print(syn0)
+print()
+print("Final Output Dataset :")
+print(l1)
+
